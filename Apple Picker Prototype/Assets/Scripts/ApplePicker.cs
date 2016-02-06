c551a808f31ed72529feedee8feb78c470b6cc4d
htran48 <htran48@uwo.ca>
2016-02-05 19:25:47 -0500

    Apple Picker Prototype

---
+++ Apple Picker Prototype/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour {
+
+	public GameObject basketPrefab;
+	public int numBaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSpacingY = 2f;
+	public List<GameObject> basketList;
+
+	// Use this for initialization
+	void Start () {
+		basketList = new List <GameObject> ();
+		for (int i = 0; i < numBaskets; i++) {
+			GameObject tBasketGO = Instantiate (basketPrefab) as GameObject;
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			tBasketGO.transform.position = pos;
+			basketList.Add (tBasketGO);
+		}
+	
+	}
+	public void AppleDestroyed() {
+		//Destroy all of the falling Apples
+		GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+		foreach (GameObject tGO in tAppleArray) {
+			Destroy (tGO);
+		}
+
+		//Destroy one of the baskets
+		int basketIndex = basketList.Count-1;//index of last basket
+		//get a reference to that basket GameObject
+		GameObject tBasketGo = basketList[basketIndex];
+		//Remove the basket from the List and destroy the GameObject
+		basketList.RemoveAt(basketIndex);
+		Destroy (tBasketGo);
+
+		//Restart the game
+		if (basketList.Count == 0) {
+			SceneManager.LoadScene ("_Scene_0");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Apple Picker Prototype/Assets/Scripts/HighScore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+	static public int score = 1000;
+
+	void Awake() {
+		//If the ApplePickerHighScore alread exists, read it
+		if (PlayerPrefs.HasKey ("ApplePickerHighScore")) {
+			score = PlayerPrefs.GetInt ("ApplePickerHighScore");
+		}
+		//Assignthehigh score to ApplePickerHighScore
+		PlayerPrefs.SetInt("ApplePickerHighscore", score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Text gt = this.GetComponent<Text> ();
+		gt.text = "High Score: " + score;
+
+		//update ApplePickerHighScore in PlayerPrefs if neccessary
+		if (score > PlayerPrefs.GetInt ("ApplePickerHighScore")) {
+			PlayerPrefs.SetInt ("ApplePickerHighScore", score);
+		}
+	}
+}
